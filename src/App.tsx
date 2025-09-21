8adc7b5f409ea153fed92b7e229156a60368d4f3
ZukaiGong <zukai524@163.com>
2025-09-21 14:46:36 +0800

    feat: 开始 暂停 按钮

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { StoreProvider } from "@/store/index.tsx";
 import Matrix from "./components/Matrix";
+import ControlButtons from "./components/ControlButtons";
 
 function App() {
   return (
     <>
       <StoreProvider>
         <div
-          style={{ display: "flex", flexBasis: 0, justifyContent: "center" }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
           <div
             style={{ display: "flex", margin: "32px", background: "#9ead86" }}
           >
             <Matrix />
           </div>
+
+          <ControlButtons />
         </div>
       </StoreProvider>
     </>
---
+++ src/components/ControlButtons/index.tsx
@@ -0,0 +1,39 @@
+import { useStore, useStoreDispatch } from "@/store/index";
+import useGameStates from "@/control/states";
+
+import style from "./index.module.css";
+
+export default function ControlButtons() {
+  const store = useStore();
+  // const storeDispatch = useStoreDispatch();
+  const gameStates = useGameStates();
+
+  // 开始或结束游戏
+  function handleStartOrOver() {
+    if (!store.moveBlock) gameStates.start();
+    else {
+      gameStates.overStart();
+      gameStates.overEnd();
+    }
+  }
+
+  // 切换暂停
+  function handleTogglePause() {
+    gameStates.pause(!!store.pause);
+  }
+
+  return (
+    <>
+      <div className={style["buttons-wrapper"]}>
+        <button onClick={handleStartOrOver}>
+          {store.moveBlock ? "结束" : "开始"}
+        </button>
+        {store.moveBlock && (
+          <button onClick={handleTogglePause}>
+            {store.pause ? "继续" : "暂停"}
+          </button>
+        )}
+      </div>
+    </>
+  );
+}
